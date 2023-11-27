be805a54ec1eb8fe67a446b954a35779eb461063
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-11-27 12:42:00 -0500

    imp

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from word_list import vocab_list
 pygame.init()
 window = pygame.display.set_mode([850,850])
 
@@ -6,16 +7,16 @@ playing = True
 
 font = pygame.font.Font('freesansbold.ttf', 32)
 
-text=font.render('Tutorials',True, (0,255,0),(0,0,128))
+text=font.render(vocab_list[0][0],True, (0,255,0),(0,0,128))
 
 textRect=text.get_rect()
 
-textRect.center=(450//2,450//2)
+textRect.center=(850//2,850//3)
 
 while playing:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            playing ==False
+            playing =False
 
     window.fill((255, 255, 255))
     # window.blit(image, (0,0))
