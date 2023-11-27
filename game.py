2fe7962b5268f8681e4b77c04780ae2f376c18c1
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-11-27 17:32:02 -0500

    Ian, congrats!

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
+from user import User
 import pygame
 from play_game import play_game
 from word_list import vocab_list
-
 
 pygame.init()
 window = pygame.display.set_mode([850, 500])
@@ -9,7 +9,7 @@ window = pygame.display.set_mode([850, 500])
 playing = True
 
 while playing:
-    play_game('Ian', vocab_list, window)
+    playing = play_game(User("Ian"), vocab_list, window)
     window.fill((255, 255, 255)) 
     pygame.display.update()
 
